97d04a9958ae1a478af26495b7904e7295e39016
Lyudmila17 <merkel.naller@gmail.com>
2018-07-14 15:48:07 +0000

    athene: configs: Remove _matrix and add more HALs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,7 +206,6 @@ TARGET_USE_SDCLANG := true
 
 # Manifest
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
-DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/configs/compatibility_matrix.xml
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
