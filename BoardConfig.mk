7e993be767445c742016c4fffe227d3c6144536f
Lyudmila17 <merkel.naller@gmail.com>
2018-07-15 20:13:55 +0000

    athene: config: Use compatibility_matrix.xml

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,6 +206,7 @@ TARGET_USE_SDCLANG := true
 
 # Manifest
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/compatibility_matrix.xml
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
