20758b8de4acbf8d2319eb5a66b5174b8c642bc9
Lyudmila17 <merkel.naller@gmail.com>
2018-08-11 16:28:01 +0000

    athene: hidl: Re-make hidl

--- hidl.mk
+++ hidl.mk
@@ -6,27 +6,39 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
 	android.hardware.audio@2.0-impl \
+	android.hardware.audio@2.0-service \
 	android.hardware.audio.effect@2.0-impl \
 	android.hardware.broadcastradio@1.0-impl \
 	android.hardware.soundtrigger@2.0-impl
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+	android.hardware.bluetooth@1.0-impl \
+	android.hardware.bluetooth@1.0-service
+
 # Camera
 PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl \
+	android.hardware.camera.provider@2.4-service \
 	camera.device@3.2-impl
+
+# Configstore
+PRODUCT_PACKAGES += \
+	android.hardware.configstore@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
 	android.hardware.graphics.allocator@2.0-impl \
 	android.hardware.graphics.allocator@2.0-service \
-	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.graphics.composer@2.1-impl \
-	android.hardware.graphics.composer@2.1-service
+	android.hardware.graphics.composer@2.1-service \
+	android.hardware.graphics.mapper@2.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \
 	android.hardware.drm@1.0-impl \
-	android.hardware.drm@1.0-service
+	android.hardware.drm@1.0-service \
+	android.hardware.drm@1.0-service.widevine
 
 # Fingerprint
 PRODUCT_PACKAGES += \
@@ -34,27 +46,28 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
-	android.hardware.gatekeeper@1.0-impl
+	android.hardware.gatekeeper@1.0-impl \
+	android.hardware.gatekeeper@1.0-service
 
-# GPS
-PRODUCT_PACKAGES += \
-	android.hardware.gnss@1.0-impl
+# GNSS
+#PRODUCT_PACKAGES += \
+	#android.hardware.gnss@1.0-impl \
+	#android.hardware.gnss@1.0-service
 
 # Vibrator
 PRODUCT_PACKAGES += \
-	android.hardware.vibrator@1.0-impl
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-	android.hardware.bluetooth@1.0-impl
+	android.hardware.vibrator@1.0-impl \
+	android.hardware.vibrator@1.0-service
 
 # Memtrack HAL
 PRODUCT_PACKAGES += \
-	android.hardware.memtrack@1.0-impl
+	android.hardware.memtrack@1.0-impl \
+	android.hardware.memtrack@1.0-service
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-	android.hardware.keymaster@3.0-impl
+	android.hardware.keymaster@3.0-impl \
+	android.hardware.keymaster@3.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
@@ -62,7 +75,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-	android.hardware.light@2.0-impl
+	android.hardware.light@2.0-impl \
+	android.hardware.light@2.0-service
 
 # Netutils
 PRODUCT_PACKAGES += \
@@ -76,19 +90,23 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.renderscript@1.0-impl
 
+# RIL
+PRODUCT_PACKAGES += \
+	android.hardware.radio@1.0
+
 # Sensors
 PRODUCT_PACKAGES += \
-	android.hardware.sensors@1.0-impl
-
-# Thermal
-PRODUCT_PACKAGES += \
-   android.hardware.thermal@1.1-impl \
-   android.hardware.thermal@1.1-service
+	android.hardware.sensors@1.0-impl \
+	android.hardware.sensors@1.0-service
 
 # Health
 PRODUCT_PACKAGES += \
 	android.hardware.health@1.0-impl \
 	android.hardware.health@1.0-service
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-service
 
 # WiFi
 PRODUCT_PACKAGES += \
