6345817603d9a19d63175a57dffec9f1169a681f
Lyudmila17 <merkel.naller@gmail.com>
2018-08-11 22:13:22 +0000

    athene: device: Fix thermal engine conf

--- device.mk
+++ device.mk
@@ -282,7 +282,7 @@ PRODUCT_PACKAGES += \
     thermal.msm8952
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-athene.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-athene.conf
+    $(LOCAL_PATH)/configs/thermal-engine-athene.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # VNDK-SP:
 PRODUCT_PACKAGES += \
