497482a34d6bc6c2d0b8fc9242b24ade05234feb
Lyudmila17 <merkel.naller@gmail.com>
2018-08-16 17:56:42 +0000

    athene: Update IMS shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,38 +16,27 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := MediaBuffer.c
-
 LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
-
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := rild_socket.c
-
 LOCAL_MODULE := rild_socket
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
-
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := get_process_name.c
-
 LOCAL_MODULE := libshims_get_process_name
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
-
 include $(BUILD_SHARED_LIBRARY)
 
-# RIL
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := sensor.cpp
 LOCAL_SHARED_LIBRARIES := libgui libsensor
