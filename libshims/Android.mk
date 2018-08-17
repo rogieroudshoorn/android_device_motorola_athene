863f9101d52bad4155d4d79f307aaa843f49446e
Lyudmila17 <merkel.naller@gmail.com>
2018-08-17 13:02:08 +0000

    athene: libshims: Fix building

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -17,7 +17,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := MediaBuffer.cpp
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_SHARED_LIBRARIES := libui libgui libstagefright_foundation
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
