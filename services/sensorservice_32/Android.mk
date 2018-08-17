84f2ab5a92b9f10e13f2b5086c11a5340453b117
Lyudmila17 <merkel.naller@gmail.com>
2018-08-17 17:31:12 +0000

    athene: services: sensorservice: Fix path

--- services/sensorservice_32/Android.mk
+++ services/sensorservice_32/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= \
-	../../../../frameworks/native/services/sensorservice/main_sensorservice.cpp
+	../../../../../frameworks/native/services/sensorservice/main_sensorservice.cpp
 LOCAL_SHARED_LIBRARIES := \
 	libsensorservice \
 	libbinder \
