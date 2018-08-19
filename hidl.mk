bbfd5569185b393e038447cb1b2d9850216a8ffb
Lyudmila17 <merkel.naller@gmail.com>
2018-08-19 16:12:21 +0000

    athene: services: Remove camera_provider from services

--- hidl.mk
+++ hidl.mk
@@ -19,7 +19,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl \
-	android.hardware.camera.provider@2.4-service_athene \
+	android.hardware.camera.provider@2.4-service \
 	camera.device@3.2-impl
 
 # Configstore
