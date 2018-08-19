1a670849ec54724f369dd1d1f0d228eb4acac24e
Lyudmila17 <merkel.naller@gmail.com>
2018-08-19 16:12:55 +0000

    athene: hidl: Add camera 1.0 impl

--- hidl.mk
+++ hidl.mk
@@ -20,7 +20,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl \
 	android.hardware.camera.provider@2.4-service \
-	camera.device@3.2-impl
+	camera.device@3.2-impl \
+	camera.device@1.0-impl
 
 # Configstore
 PRODUCT_PACKAGES += \
