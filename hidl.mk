a6ee85309942e7429f856d484eca97850bb62481
Lyudmila17 <merkel.naller@gmail.com>
2018-08-27 00:29:36 +0000

    athene: Ship again sensors

--- hidl.mk
+++ hidl.mk
@@ -99,8 +99,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-	android.hardware.sensors@1.0-impl
-	#android.hardware.sensors@1.0-service
+	android.hardware.sensors@1.0-impl \
+	android.hardware.sensors@1.0-service
 
 # Health
 PRODUCT_PACKAGES += \
